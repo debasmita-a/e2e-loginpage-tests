b5ccfced9fe280147db3d75f439fba909b16c487
Debasmita A <72942875+debasmita-a@users.noreply.github.com>
2023-12-22 07:06:53 +0100

    added report listeners

--- src/main/java/com/qa/login/factory/DriverFactory.java
+++ src/main/java/com/qa/login/factory/DriverFactory.java
@@ -1,20 +1,24 @@
 package com.qa.login.factory;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 
 public class DriverFactory {
 
 	private WebDriver driver;
 	private Properties prop;
-	private ThreadLocal<WebDriver> tlDriver;
+	private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
 	
 	public WebDriver initDriver(Properties prop) {
 		
@@ -48,7 +52,7 @@ public class DriverFactory {
 		return driver;
 	}
 	
-	public WebDriver getDriver() {
+	public static WebDriver getDriver() {
 		return tlDriver.get();
 	}
 	
@@ -64,5 +68,20 @@ public class DriverFactory {
 		}
 		
 		return prop;
+	}
+	
+	/**
+	 * take screenshot
+	 */
+	public static String getScreenshot() {
+		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
+		File destination = new File(path);
+		try {
+			FileHandler.copy(srcFile, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return path;
 	}
 }
